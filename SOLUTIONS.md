58a70ae3c1f71fbe8c92905abd7e5bda0a73c479
Daniel Ramón Gallardo <daniel.ramon@minorisa.net>
2021-11-18 11:12:53 +0100

    Challenge 1 solved.

---
+++ SOLUTIONS.md
@@ -0,0 +1,12 @@
+#Solutionary
+
+---
+
+This document have the pourpouse of justify the solution of each challenge. It is divided by challenge
+
+
+## Challenge 1
+
+---
+
+### Solution commit: 
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 
-from src.mongoflask  import MongoJSONEncoder, ObjectIdConverter, find_restaurants
+from src.mongoflask import MongoJSONEncoder, ObjectIdConverter, find_restaurants
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = environ.get("MONGO_URI")
@@ -23,7 +23,8 @@ def restaurants():
 @app.route("/api/v1/restaurant/<id>")
 def restaurant(id):
     restaurants = find_restaurants(mongo, id)
-    return jsonify(restaurants)
+    return jsonify(restaurants) if restaurants else ("", 204)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False, port=8080)
